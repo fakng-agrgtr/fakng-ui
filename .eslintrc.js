d852944c4e4b58e4f7fad32987e8fc1faba7c920
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-08-14 15:26:36 +0600

    Added plugin for the sorting import/export.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,12 @@ module.exports = {
   },
   plugins: [
     'react',
+    'simple-import-sort',
   ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'no-unused-vars': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
   },
 };
