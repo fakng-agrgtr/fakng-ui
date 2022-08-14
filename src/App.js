eacff29d80e46d6d2b6f956560b1eda06422086b
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-08-14 17:19:05 +0600

    feat: install react-router-dom dependency, create routing and add config to .babelrc file

--- src/App.js
+++ src/App.js
@@ -2,27 +2,14 @@ import './App.css';
 
 import React from 'react';
 
-import logo from './logo.svg';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Routes> */}
+      {/* <Route path="/" element={<Home />} /> */}
+      {/* <Route path="careers" element={<Careers />} /> */}
+      {/* </Routes> */}
+      <h1>Hello World</h1>
     </div>
   );
 }
