e5ee2982af6f0492626a9530d830758e15315e4b
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-08-21 14:28:18 +0600

    Removed the menu in the header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,8 @@
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-
-const pages = [
-  { name: 'Home', path: '/' },
-  { name: 'Careers', path: '/careers' },
-];
 
 function App() {
   return (
@@ -35,18 +27,6 @@ function App() {
           >
             FAKNG AGRGTR
           </Typography>
-          <Box sx={{ display: 'flex' }}>
-            {pages.map(({ name, path }) => (
-              <Link key={path} to={path}>
-                <Button
-                  key={name + path}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  {name}
-                </Button>
-              </Link>
-            ))}
-          </Box>
         </Toolbar>
       </Container>
     </AppBar>
